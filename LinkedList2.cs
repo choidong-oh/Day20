8d1e3fe99f98f942ce919d2e2852fedfcaa2f79d
choidong-oh <qltjfeo@naver.com>
2024-12-17 13:55:46 +0900

    처음
    수업

---
+++ LinkedList2.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    //T, 제네릭
+    class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+        public Node(T value)
+        {
+            Value = value;
+            Next = null;
+        }
+    }
+
+
+    //노드를 추가하거나 삭제하는 걸 도와주는 관리 클래스
+    //마치 아이템 클래스가 있고, 이를 관리하는 Inventory클래스가 있는 것과 같음
+    //시작과 끝이 있어야함. 시작은 Head, 끝은 Tail, Count 노드 갯수
+    internal class LinkedList2<T>
+    {
+        public Node<T> Head { get; private set; }//시작점 노드 하나 기억시킬 헤드
+        public int Count { get; private set; } //노드 갯수 보관할 Count
+
+        public LinkedList2()
+        {
+            Head = null;
+            Count = 0;
+        }
+
+
+        //노드 추가
+        public void AddLast(T value)
+        {
+            Node<T> newNode = new Node<T>(value);
+
+            if (Head == null)//최초 실행 시,Head가 null임
+            {
+                Head = newNode;//처음 추가한 노드는 머리가 됨
+            }
+            else
+            {
+                Node<T> current = Head;//일단 current 노드에 머리담음
+
+                while (current.Next != null) //current 노드 다음 노드가 비지 않는다면
+                {
+                    current = current.Next;//current 노드는 다음 노드
+                }
+                current.Next = newNode;
+            }
+            Count++;
+        }
+
+        //node 제거
+        //지정된 값을 가진 노드 제거
+        //삭제 성공 실패 여부를 bool로 반환
+        public bool Remove(T value)
+        {
+            if (Head == null) //하나도 없으면 그냥 false반환
+            {
+                return false;
+            }
+
+            if (Head.Value.Equals(value))
+            {
+                Head = Head.Next;//머리가 가리키고 있던 다음 대상이 이제 머리임
+                Count--;//원래 카운트 1이엇다면 0으로
+                return true; //삭제 성공
+            }
+
+            Node<T> current = Head;
+
+            //넥스트가 비지 않았다면 게쏙 다음 노드 이동,
+            //넥스트의 값이 내가 찾는 값과 같다 하면 멈춤
+            while (current.Next != null && current.Next.Value.Equals(value))
+            {
+                current = current.Next;
+
+
+            }
+            if (current.Next == null)
+            {
+                return false; //while 다 돌아도 값이 없으면 false
+            }
+
+            current.Next = current.Next.Next;// 다음줄거를 => 다음다음한테 준다 //중간값 사라짐
+            Count--;
+            return true;
+        }
+
+        public Node<T> Find(T value)
+        {
+            Node<T> current = Head;
+            while (current.Next != null)
+            {
+                if (current.Next.Value.Equals(value))
+                {
+                    return current;
+                }
+                current = current.Next;
+
+            }
+            return null;
+
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20
+{
+    class mCharacter
+    {
+        public mWeapon wpn;
+        public string Name { get; set; }
+        public mCharacter(string wpnName)
+        {
+            this.Name = wpnName;
+        }
+
+        public mCharacter()
+        {
+            this.Name = wpn.Name;
+        }
+    }
+    class mWeapon
+    {
+        public string Name = null;
+    }
+
+
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LinkedList 링크/연결
+            //List
+            //길이가 자유롭다
+            //데이터가 연속적으로 보관되어 있기에 인덱스로 접근이 가능
+            //필요할 경우 앞에서부터 빠르게 뒤까지 검색 가능
+
+            //단점 : 
+            //중간 삽입삭제가 느림
+
+            //LinkedList
+            //데이터와 데이터가 연속으로 연결되어 있지 않고, 데이터는 각 다음 혹은
+            //이전 데이터가 어디있는지 주소를 들고 있는 방식
+            //중간 삽입 삭제가 빠름
+            //특정 요소를 검색할 떄 느림
+            //애니팡처럼 중간에 겁나 삭제 되는 그런것들에 유리
+            //NPC 이동 경로와 같이 수시로 담을 내용이 바뀔 수 있는 그런거
+
+            //LinkedList<int> list = new LinkedList<int>();
+            //LinkedList2<int> list2 = new LinkedList2<int>();
+
+            //list2.AddLast(123);
+            //list2.AddLast(456);
+            //list2.AddLast(789);
+
+            ////모든 노드 출력
+            //Node<int> current = list2.Head;
+
+            //while (current !=null)
+            //{
+            //    Console.WriteLine(current.Value);
+            //    current = current.Next;
+            //}
+
+
+            //Node<int> foundNode = list2.Find(123);
+            //Console.WriteLine(foundNode.Value);
+            /////////////////////////////////////////
+            //nullable , 널이 가능한
+            //여전히 값 타입
+            //int? a = null;//널도 담을 수 있는// 널러블 인트가 되었음
+            //bool? b = null;
+
+            //if (a.HasValue)
+            //{
+            //    Console.WriteLine("값은"+a.Value + "입니다");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("값이 없습니다");
+            //} 
+            /////////////////////////////////////////////////////
+            //삼항 연산자
+            //int toExamaine = 10;
+            //bool isBiggerThanFive;
+            //if(toExamaine > 5)
+            //{
+            //    isBiggerThanFive = true;
+            //}
+            //else
+            //{
+            //    isBiggerThanFive = false;
+            //}
+
+            //isBiggerThanFive = (toExamaine>5) ? true : false;
+            //////////////////////////////////////////////////////
+            //?연산자
+            //null들어갈수있는거?하고싶은거 //null이면 사라짐,null이 아니면 하고싶은거함
+            //mCharacter newCar = new mCharacter { Name = "이름" };//이니셜라이저 사용
+            //Console.WriteLine(newCar?.Name);
+            //Console.WriteLine(newCar?.wpn?.Name);
+
+            //////////////////////////////////////////////////////////
+            ///??연산자
+            ///??기준 좌측에 있는 형식이 만약 null이라면 ??의 우측에 있는 내용,
+            /////null이 아니었다면 ?? 기준 좌측에 있는 기능 사용
+            
+            //List<mCharacter> myTroop = new List<mCharacter>();
+
+            //myTroop.Add(new mCharacter());
+            //myTroop.Add(new mCharacter("창"));
+            //myTroop.Add(new mCharacter());
+            //myTroop.Add(new mCharacter("방패"));
+
+            //foreach (var myUnit in myTroop)
+            //{
+            //    Console.WriteLine(myUnit.wpn.Name ?? "무기가 없습니다");
+
+            //}
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
